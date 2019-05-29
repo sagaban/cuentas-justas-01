df474f9c4cd5b60ece007f1cc487764cc47557ea
Santiago Bandiera <sagaban@hotmail.com>
2019-05-29 12:28:46 -0300

    Add Quasar

--- src/main.js
+++ src/main.js
@@ -4,6 +4,49 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
+import "./styles/quasar.styl";
+import lang from "quasar/lang/es.js";
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import {
+  Quasar,
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel
+} from "quasar";
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel
+  },
+  directives: {},
+  plugins: {},
+  lang: lang
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
